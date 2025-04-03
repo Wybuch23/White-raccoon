64b1d266a28238310971e9d1b12961417ac005c4
Wybuch <tsudnya@gmail.com>
2025-04-03 16:37:14 +0300

    bugs fix 3 april 0.2

--- src/js/modules/mobileNavigation.js
+++ src/js/modules/mobileNavigation.js
@@ -1,10 +1,11 @@
-import { fadeIn, fadeOut } from "./fade.js"
+import { fadeIn, fadeOut } from "./fade.js";
 
 export function setupMobileNavigation() {
     const toggleBtn = document.getElementById("toggle-btn-header-mobile");
     const iconDots = document.getElementById("icon-dots-vertical-header-mobile");
     const iconClose = document.getElementById("icon-close-header-mobile");
     const nav = document.getElementById("navigation-switch-header-mobile");
+    const body = document.body; // Получаем body
 
     if (!toggleBtn || !iconDots || !iconClose || !nav) return;
 
@@ -13,9 +14,13 @@ export function setupMobileNavigation() {
         iconClose.classList.toggle("active");
 
         if (nav.classList.contains("active")) {
-            fadeOut(nav, () => nav.classList.remove("active"));
+            fadeOut(nav, () => {
+                nav.classList.remove("active");
+                body.style.overflow = ""; // Возвращаем прокрутку
+            });
         } else {
             nav.classList.add("active");
+            body.style.overflow = "hidden"; // Отключаем прокрутку
             fadeIn(nav);
         }
     });
