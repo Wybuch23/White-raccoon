ba892db779cddd9d83414c873d9fbea0737d5c16
Wybuch <tsudnya@gmail.com>
2025-04-03 18:11:48 +0300

    bugs fix 3 april 0.3 heder hide new js

--- src/js/modules/focusBlue.js
+++ src/js/modules/focusBlue.js
@@ -1,4 +1,40 @@
 // // добавление класса focus-blue
+// export function setupFocusBlue() {
+//     const allCards = document.querySelectorAll(".basic__card-primary");
+//     const header = document.querySelector(".header");
+//     const targetElementsSelector = ".header, .basic__card-primary, .basic__card-img, .basic__card-title, .basic__sticky-title, .basic__top-title, body, .basic__card-icon, .basic__card-title, .basic__card-text, .card-tabs-img, .tabs-control__tub, .btn-primary, .card-tabs-img__text";
+
+//     const getTargetElements = () => document.querySelectorAll(targetElementsSelector);
+
+//     let hoverTimeout, leaveTimeout;
+
+//     allCards.forEach(card => {
+//         card.addEventListener("mouseenter", () => {
+//             clearTimeout(leaveTimeout);
+
+//             hoverTimeout = setTimeout(() => {
+//                 getTargetElements().forEach(el => el.classList.add("focus-blue"));
+//                 card.querySelectorAll(targetElementsSelector).forEach(el => el.classList.remove("focus-blue"));
+
+//                 // Скрываем хедер, если он ещё не скрыт
+//                 if (!header.classList.contains("hide")) {
+//                     header.classList.add("hide");
+//                 }
+//             }, 200);
+//         });
+
+//         card.addEventListener("mouseleave", () => {
+//             clearTimeout(hoverTimeout);
+
+//             leaveTimeout = setTimeout(() => {
+//                 if (![...allCards].some(card => card.matches(":hover"))) {
+//                     getTargetElements().forEach(el => el.classList.remove("focus-blue"));
+//                 }
+//             }, 200);
+//         });
+//     });
+// }
+
 export function setupFocusBlue() {
     const allCards = document.querySelectorAll(".basic__card-primary");
     const header = document.querySelector(".header");
@@ -16,8 +52,8 @@ export function setupFocusBlue() {
                 getTargetElements().forEach(el => el.classList.add("focus-blue"));
                 card.querySelectorAll(targetElementsSelector).forEach(el => el.classList.remove("focus-blue"));
 
-                // Скрываем хедер, если он ещё не скрыт
-                if (!header.classList.contains("hide")) {
+                // Если хедер не скрыт изначально (не имеет только класса .header), скрываем его при наведении
+                if (!header.classList.contains("hide") && !header.classList.contains("header")) {
                     header.classList.add("hide");
                 }
             }, 200);
@@ -29,8 +65,14 @@ export function setupFocusBlue() {
             leaveTimeout = setTimeout(() => {
                 if (![...allCards].some(card => card.matches(":hover"))) {
                     getTargetElements().forEach(el => el.classList.remove("focus-blue"));
+
+                    // Если хедер был скрыт, показываем его, если он не имеет класса .header (если он скрыт при наведении)
+                    if (header.classList.contains("hide") && !header.classList.contains("header")) {
+                        header.classList.remove("hide");
+                    }
                 }
             }, 200);
         });
     });
 }
+
--- src/js/modules/scrollHeader.js
+++ src/js/modules/scrollHeader.js
@@ -1,3 +1,22 @@
+// /* Скрытие шапки при скролле */
+// export function setupScrollHeader() {
+//     let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+//     const header = document.querySelector(".header");
+//     const scrollThreshold = 160;
+
+//     window.addEventListener("scroll", () => {
+//         let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+//         if (scrollTop > lastScrollTop + scrollThreshold) {
+//             header.classList.add("hide");
+//             lastScrollTop = scrollTop; // Обновляем только после скрытия
+//         } else if (scrollTop < lastScrollTop - scrollThreshold) {
+//             header.classList.remove("hide");
+//             lastScrollTop = scrollTop; // Обновляем только после появления
+//         }
+//     });
+// }
+
 /* Скрытие шапки при скролле */
 export function setupScrollHeader() {
     let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
@@ -7,7 +26,8 @@ export function setupScrollHeader() {
     window.addEventListener("scroll", () => {
         let scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-        if (scrollTop > lastScrollTop + scrollThreshold) {
+        // Добавляем проверку на наличие класса 'focus-blue'
+        if (scrollTop > lastScrollTop + scrollThreshold && !header.classList.contains("focus-blue")) {
             header.classList.add("hide");
             lastScrollTop = scrollTop; // Обновляем только после скрытия
         } else if (scrollTop < lastScrollTop - scrollThreshold) {
